20f96f4f9ad1aaf7ba1d51d82a7c9e27db14901e
Phybelife <2671576531@qq.com>
2024-01-03 18:52:35 +0800

    homepage

---
+++ cn/nav.js
@@ -0,0 +1,8 @@
+document.writeln("		<nav>");
+document.writeln("    		<ul>");
+document.writeln("        		<li><a href=\"../cn/index.html\">主页</a></li>");
+document.writeln("        		<li><a href=\"doc.html\">文档</a></li>");
+document.writeln("        		<li><a href=\"study.html\">学习指南</a></li>");
+document.writeln("        		<li><a href=\"links.html\">链接</a></li>");
+document.writeln("    		</ul>");
+document.writeln("		</nav>");
